a61a9bdf8e1db11ab9abaf525688b4a0b4cbc0ae
Abdul Shabbir <abdulqshabbir@gmail.com>
2017-12-24 15:43:52 +0300

    added 'clear screen' functionality by iterating over squares that were black and removing that class

--- script.js
+++ script.js
@@ -2,10 +2,19 @@ let isGridCreated = false;
 
 const grid = document.getElementById('gridContainer');
 const newGridButton = document.getElementById('controlGridSizeButton');
+const clearScreenButton = document.getElementById('clearScreenButton');
 
 newGridButton.addEventListener('click', createSquares);
+clearScreenButton.addEventListener('click', removeAllSquares);
 
-
+function removeAllSquares() {
+  if(isGridCreated === true) {
+    const squares = Array.from(document.querySelectorAll('.changeToBlack'));
+    squares.forEach((square) =>{
+      square.classList.remove('changeToBlack'); 
+    });
+  }
+}
 
 //assumption: we have a 300px by 500px main container which will create
 // 1500 10 by 10 squares
