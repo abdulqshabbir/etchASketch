406b5c0af4b6fb0fd2ac9a3df7e5487378a80364
Abdul Shabbir <abdulqshabbir@gmail.com>
2017-12-24 15:32:13 +0300

    Event listener added to each square on the grid which changes background color to black when mouse enters

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
+let isGridCreated = false;
+
 const grid = document.getElementById('gridContainer');
 const newGridButton = document.getElementById('controlGridSizeButton');
 
-newGridButton.addEventListener('click', (e) => {
-  for (let i = 0; i < 1500; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square')
-    grid.appendChild(square);
+newGridButton.addEventListener('click', createSquares);
+
+
+
+//assumption: we have a 300px by 500px main container which will create
+// 1500 10 by 10 squares
+function createSquares(e) {
+  if(isGridCreated === false) {
+    for (let i = 1; i <= 1500; i++) {
+      const square = document.createElement('div');
+      square.setAttribute('id', `squareNum${i}`);
+      square.setAttribute('class', `square`);
+      grid.appendChild(square);
+      isGridCreated = true;
+    }
   }
-});
+  const squares = Array.from(document.querySelectorAll('.square'));
+  highlightOnHover(squares);
+}
+
+function highlightOnHover(squares) {
+  squares.forEach(function(square) {
+    square.addEventListener('mouseenter', changeToBlack);
+  });
+}
+
+function changeToBlack() {
+  this.classList.add('changeToBlack');
+}
